db1bc31fd9d9859e074fea938fedfcd05cca563c
Elliot <elliotfaugier@gmail.com>
2024-05-31 16:24:38 +0200

    push

---
+++ backend/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+huggingface-cli login --token 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
+
+# apt update -y
+# apt upgrade -y
+# apt install wget -y
+
+# apt remove --purge *nvidia*
+# apt remove --purge* cuda*
+# apt autoremove -y
+
+
+# wget https://developer.download.nvidia.com/compute/cuda/12.5.0/local_installers/cuda_12.5.0_555.42.02_linux.run
+# sh cuda_12.5.0_555.42.02_linux.run
+
+
+nvidia-smi
+
+exec "$@"
